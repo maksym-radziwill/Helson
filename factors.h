231f185c2a105d7b01660235c0ce424d4f5cf048
Maksym Radziwill <maksym.radziwill@gmail.com>
2015-11-08 22:13:49 -0500

    Simple implementation of Pollard rho's algorithm

---
+++ factors.h
@@ -0,0 +1,8 @@
+#include <vector>
+/* Factorization functions */
+extern int gcd(int a, int b);
+extern int trivial_factor(int n);
+extern void factorize(std::vector<int> & factors, int n);
+extern int factor(int);
+/* ----------------------- */
+
